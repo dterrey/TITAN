dc4772256a6de3ff25413b0bb4d3cd491df4cd1e
dterrey <117341487+dterrey@users.noreply.github.com>
2024-10-04 00:23:07 +1000

    Create app.py

---
+++ titan/TITAN_IOC/app.py
@@ -0,0 +1,8 @@
+from app import app, db  # Import the db instance
+
+# Create the tables if they don't exist
+with app.app_context():
+    db.create_all()
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5112)
