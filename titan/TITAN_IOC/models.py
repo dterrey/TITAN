eebef249b2d549ca3e63aaf171f53868e695135c
dterrey <117341487+dterrey@users.noreply.github.com>
2024-10-10 17:51:55 +1100

    Update models.py
    Fixed SQL DB

--- titan/TITAN_IOC/models.py
+++ titan/TITAN_IOC/models.py
@@ -1,6 +1,4 @@
-
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -8,4 +6,6 @@ class IOC(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     indicator = db.Column(db.String(255), nullable=False)
     type = db.Column(db.String(50), nullable=False)
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    timestamp = db.Column(db.DateTime, default=db.func.current_timestamp())
+    tag = db.Column(db.String(255), nullable=True)
+    new_column = db.Column(db.String(255), nullable=True)  # Temporary column
