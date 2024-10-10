6fadb63522fa730dca573d10d324b94f7975ba7e
dterrey <117341487+dterrey@users.noreply.github.com>
2024-10-11 00:53:57 +1100

    Update models.py

--- titan/TITAN_IOC/models.py
+++ titan/TITAN_IOC/models.py
@@ -3,9 +3,18 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class IOC(db.Model):
+    __tablename__ = 'ioc'
     id = db.Column(db.Integer, primary_key=True)
     indicator = db.Column(db.String(255), nullable=False)
     type = db.Column(db.String(50), nullable=False)
     timestamp = db.Column(db.DateTime, default=db.func.current_timestamp())
     tag = db.Column(db.String(255), nullable=True)
-    new_column = db.Column(db.String(255), nullable=True)  # Temporary column
+
+class CodexIOC(db.Model):
+    __bind_key__ = 'codex'
+    __tablename__ = 'codex_ioc'
+    id = db.Column(db.Integer, primary_key=True)
+    indicator = db.Column(db.String, nullable=False)
+    type = db.Column(db.String, nullable=False)
+    parsed_file = db.Column(db.String, nullable=False)  # Track which file the IOC came from
+    timestamp = db.Column(db.DateTime, default=db.func.current_timestamp())
