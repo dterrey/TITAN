84ec8abfdbfd7cc5139ab6b34c736a52527adbbb
dterrey <117341487+dterrey@users.noreply.github.com>
2024-10-11 00:54:14 +1100

    Update config.py

--- titan/TITAN_IOC/config.py
+++ titan/TITAN_IOC/config.py
@@ -1,6 +1,9 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///ioc_database.db'  # SQLite for local development
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///ioc_database.db'  # Main database (User IOCs)
+    SQLALCHEMY_BINDS = {
+        'codex': 'sqlite:///codex_db.db'  # Secondary database for Codex IOCs
+    }
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.urandom(24)
